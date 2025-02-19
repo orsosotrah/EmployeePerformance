e25f659abda9d714e4292489180097d8fb668f8f
sosro <sosro.hartomo@polyrific.com>
2025-02-19 22:10:00 +0700

    BuildingBlocks Init

---
+++ src/BuildingBlocks/BuildingBlocks.Contracts/Queries/IQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Contracts.Queries
+{
+    public interface IQueryHandler<in TQuery, TResponse>
+        where TQuery : IQuery<TResponse>
+    {
+        Task<TResponse> Handle(TQuery query, CancellationToken cancellationToken);
+    }
+
+    public abstract record BaseQuery<TResponse> : IQuery<TResponse>
+    {
+        public Guid Id { get; private init; }
+        public DateTime Timestamp { get; private init; }
+
+        protected BaseQuery()
+        {
+            Id = Guid.NewGuid();
+            Timestamp = DateTime.UtcNow;
+        }
+    }
+}
